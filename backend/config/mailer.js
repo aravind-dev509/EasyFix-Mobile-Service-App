1aaee8743145256718e629a7dea7db1e0742730a
aravind-dev509 <daravind958@gmail.com>
2026-06-25 11:48:45 +0530

    Update mailer.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -5,8 +5,9 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
 
   auth: {
-    user: "daravind958@gmail.com",
-    pass: "ckqydrvzmzokfgpm"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+
   }
 
 });
