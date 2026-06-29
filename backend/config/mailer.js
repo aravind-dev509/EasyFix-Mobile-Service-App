07b428e66ea15ade7737c8e110cb334e854c4718
aravind-dev509 <daravind958@gmail.com>
2026-06-29 20:02:49 +0530

    Add SMTP verification

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,10 +2,26 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+   host: "smtp.gmail.com",
+
+    port: 465,
+
+    secure: true,
+    
   auth: {
-    user: "daravind958@gmail.com",
-    pass: "xdyyduowxyeyijaf"
+    user:"daravind958@gmail.com",
+    pass:"xdyyduowxyeyijaf"
   }
+});
+
+transporter.verify(function (error, success) {
+
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+
 });
 
 module.exports = transporter;
