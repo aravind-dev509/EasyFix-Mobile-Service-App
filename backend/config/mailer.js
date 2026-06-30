39d955ae01b4c89f936f381135a74fefa6a57208
aravind-dev509 <daravind958@gmail.com>
2026-06-30 18:13:25 +0530

    Use SMTP port 465

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,15 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user:"daravind958@gmail.com",
     pass:"xdyyduowxyeyijaf"
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  }
+ 
 });
 
 module.exports = transporter;
